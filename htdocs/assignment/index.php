410eeb8002fa22a9522f0aebace5486234a717e5
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-03-20 19:18:34 +0700

    Add "Header" and "Footer"

---
+++ htdocs/assignment/index.php
@@ -0,0 +1,47 @@
+﻿<!DOCTYPE html>
+<meta charset="utf-8" />
+<html lang="en">
+
+<head>
+  <?php include_once "share/import.php" ?>
+  <title>Quiz Maker</title>
+  <link rel="icon" type="image/png" href="/img/favicon.png" />
+</head>
+
+<style>
+  #login-form {
+    width: 50%;
+    margin: 0 auto;
+  }
+</style>
+
+<body>
+  <div class="container">
+    <header>
+      <?php include_once "share/header.php" ?>
+    </header>
+    <div id="login-form">
+      <form method="POST" action="/login.php">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+        </div>
+        <div>
+          <a href="#">Forgot password?</a>
+        </div>
+        <div class="text-right">
+          <button type="submit" class="btn btn-default">Log In</button>
+        </div>
+      </form>
+    </div>
+
+
+    <footer>
+      <?php include_once "share/footer.php" ?>
+    </footer>
+  </div>
+</body>
---
+++ htdocs/assignment/share/footer.php
@@ -0,0 +1,9 @@
+﻿<div class="container-fluid">
+  <p class="copyright text-center">
+      &copy;
+      <script>
+        document.write(new Date().getFullYear())
+      </script>
+    <a href="#">QuizMaker</a>. All Rights Reserved
+  </p>
+</div>
