f0dbfc7879495ce89f3578282221f59d4004b8f9
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-03-20 15:26:47 +0700

    Add "Log In" feature

---
+++ htdocs/assignment/login.php
@@ -0,0 +1,16 @@
+﻿<?php 
+    require_once('./dbconnect.php');
+    $username = trim($_POST["username"], " \t\n\r\0\x0B");
+    $password = trim($_POST["password"], " \t\n\r\0\x0B");
+    
+    $sql = "SELECT * FROM `users` WHERE username='$username' AND password='$password'";
+
+    $result = $connection->query($sql);
+    if (!is_null($result) && $result->num_rows > 0){
+        while($row = $result->fetch_assoc()) {
+            echo "Sign in successfully <br>";
+        } 
+      } else {
+        echo "Wrong username or password";
+    }
+?>
