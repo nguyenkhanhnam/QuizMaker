ef8a699f845392442d65c288050a04ff7bb435b0
Unknown <1412817@hcmut.edu.vn>
2018-03-28 18:02:52 +0700

    update admin dasboard
    just a little bit

--- htdocs/assignment/dashboard/admin.php
+++ htdocs/assignment/dashboard/admin.php
@@ -24,12 +24,52 @@
   <div class="container">
     <header>
       <?php include_once "../share/header.php" ?>
+      <h1 class= "dashboard-name">Admin Dashboard</h1>
     </header>
     
-    
+
+    <div class="tab">
+      <button class="tablinks" onclick="openTab(event, 'add-account')">Add Account</button>
+      <button class="tablinks" onclick="openTab(event, 'find-user')">Find User</button>
+    </div>
+
+  <div id="add-account form-center" class="tabcontent">
+    <form id= "login-form" method= "POST" action= "">
+      <div class="form-group">
+        <label for="username">Username</label>
+        <div class="input-group">
+          <span class="input-group-addon">
+            <i class="fa fa-user-circle fa-lg" aria-hidden="true"></i>
+          </span>
+          <input type="text" class="form-control" id="username" placeholder="Username" name="username" required>
+        </div>
+      </div>
+    </form>
+  </div>
+
+  <div id="find-user" class="tabcontent">
+    <h3>Paris</h3>
+    <p>Paris is the capital of France.</p> 
+  </div>
 
     <footer>
       <?php include_once "../share/footer.php" ?>
     </footer>
   </div>
 </body>
+
+<script>
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+</script>
