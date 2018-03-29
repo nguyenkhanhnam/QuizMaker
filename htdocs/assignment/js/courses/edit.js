f55f67a403e8be5448d6a82293f223a76fbb51ed
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-03-29 21:21:58 +0700

    Add Edit Course UI & JS

---
+++ htdocs/assignment/js/courses/edit.js
@@ -0,0 +1,52 @@
+﻿$(document).ready(function () {
+  var path = window.location.href.split('/')
+  var courseCode = path.pop() || path.pop()
+
+  $.ajax({
+    type: 'GET',
+    url: '/api/courses/' + courseCode,
+    dataType: 'json',
+    complete: function (res) {
+      if (res.status !== 200) {
+        return displayToastWithRedirect('error', 'No course found', '/courses')
+      } else {
+        $('#code').val(res.responseJSON.code)
+        $('#name').val(res.responseJSON.name)
+      }
+    }
+  }
+  );
+
+  $('#btn-save').on('click', function () {
+    var $form = $('form')
+    //var data = getFormData($form)
+    var data = {
+      code: $('#code').val(),
+      name: $('#name').val()
+    }
+    $('form').submit(function () {
+      $.ajax({
+        url: '/api/courses/',
+        type: 'PUT',
+        contentType: 'application/json',
+        data: data,
+        complete: function (res) {
+          if (res.status !== 200) {
+            if(res.status === 400){
+              displayToast('error', 'Invalid data')
+            }
+            else if (res.status === 404) {
+              displayToastWithRedirect('error', 'Course not found', '/courses')
+            } 
+            else if (res.status === 409) {
+              displayToast('error', 'Course code existed')
+            }
+          } else {
+            displayToastWithRedirect('success', 'Course edited successfully', '/courses')
+          }
+        }
+      })
+      return false
+    })
+  })
+});
