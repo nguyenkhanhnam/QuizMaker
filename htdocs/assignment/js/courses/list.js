570ab6cbb5f7b56c3d5b40e55466509a3d021df0
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-03-29 10:56:01 +0700

    Display List Course Table

--- htdocs/assignment/js/courses/list.js
+++ htdocs/assignment/js/courses/list.js
@@ -3,34 +3,22 @@
         type: 'GET',
         url: '/api/courses',
         dataType: 'json',
-       
+
         complete: function (res) {
             if (res.status !== 200) {
                 console.log(res)
             } else {
-                console.log(res);
-                console.log(res.responseJSON[0].name);
+                var courses = res.responseJSON;
+                for (var courseIdx = 0; courseIdx < courses.length; courseIdx++) {
+                    var row = '<tr>';
+                    var col = '<td>' + courses[courseIdx].code + '</td>';
+                    col += '<td>' + courses[courseIdx].name + '</td>';
+                    row += col;
+                    row += '</tr>';
+                    $('#course-table').append(row);
+                }
             }
         }
     }
     );
 });
-
- //contentType: "application/json",
-        //dataType: "json",
-       // data: JSON.stringify({id: 1}),    
-        // success: function (res) { 
-            
-        //     console.log('meow');
-        //     console.log(res);
-        //     // $res= JSON.parse(data);
-        //     console.log(res[0].name1);
-        // },
-        // error: function (xhr, textStatus, errorThrown) {
-        //     console.log(errorThrown)
-        //     console.log(xhr)
-        //     console.log(textStatus)
-        //     if (xhr.status == 200) {
-        //         return displayToast('success', 'Listed');
-        //     }
-        // }
--- htdocs/assignment/views/courses/list.php
+++ htdocs/assignment/views/courses/list.php
@@ -26,6 +26,18 @@
     <header>
       <?php include_once "../share/header.php" ?>
     </header>
+
+    <table class="table table-striped" id="course-table">
+      <thead>
+        <tr>
+          <th>Course Code</th>
+          <th>Course Name</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+
     <footer>
       <?php include_once "../share/footer.php" ?>
     </footer>
