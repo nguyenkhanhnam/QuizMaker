21ea10fef5aa7e78c070ef370816e208c2997a5a
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-03-29 21:24:26 +0700

    Edit Course API and Validator for Course Code, Course Name

--- htdocs/assignment/api/courses.php
+++ htdocs/assignment/api/courses.php
@@ -5,18 +5,47 @@
     $data= array();
     $connection= mysqli_connect("localhost", "root", "", "assignment");
 
+    function isValidCourseCode($courseCode){
+        if (preg_match('/^[A-Z]{2}[0-9]{4}$/', $courseCode))
+            return true;
+        return false;
+    }
+
+    function isValidCourseName($courseName){
+        if (preg_match('/^[A-Za-z0-9]{50}$/', $courseName))
+            return true;
+        return false;
+    }
+
     switch($method){
         case 'GET': {
-            $sql =  "SELECT * FROM `courses`";
-            //    $response = array();
-            $result = mysqli_query($connection, $sql);
-            header('Content-Type: text/html; charset=utf-8');
-            while($row = mysqli_fetch_array($result)){
-                array_push($data, array('code' => $row["code"], 'name' => $row["name"]));
+            if(empty($_GET["code"])){
+                $sql =  "SELECT * FROM `courses`";
+                //    $response = array();
+                $result = mysqli_query($connection, $sql);
+                header('Content-Type: text/html; charset=utf-8');
+                while($row = mysqli_fetch_array($result)){
+                    array_push($data, array('code' => $row["code"], 'name' => $row["name"]));
+                }
+                echo json_encode($data);
+                break;
             }
+            else {
+                $courseCode = $_GET['code'];
 
-            echo json_encode($data);
-            break;
+                $sql =  "SELECT * FROM `courses` WHERE code='$courseCode' LIMIT 1";
+                $result = mysqli_query($connection, $sql);
+                if(mysqli_num_rows($result) > 0){
+                    while($row = mysqli_fetch_array($result)){
+                        $data = array('code' => $row["code"], 'name' => $row["name"]);
+                    }
+                    echo json_encode($data);
+                }
+                else {
+                    var_dump(http_response_code(404));
+                }
+                break;
+            }
         }
         case 'POST': {
             $code = trim($_POST["code"], " \t\n\r\0\x0B");
@@ -28,6 +57,31 @@
             } else {
                 return var_dump(http_response_code(409));
             }
+            break;
+        }
+        case 'PUT': {
+            parse_str(file_get_contents('php://input'), $_PUT);
+            $code = trim($_PUT["code"], " \t\n\r\0\x0B");
+            $name = $_PUT["name"];
+
+            if(!isValidCourseCode($code) || !isValidCourseName($name)){
+                return var_dump(http_response_code(400));
+            }
+
+            $sql =  "SELECT * FROM `courses` WHERE code='$code' LIMIT 1";
+            $result = mysqli_query($connection, $sql);
+            if(mysqli_num_rows($result) <= 0) {
+                return var_dump(http_response_code(404));
+            }
+
+            $sql = "UPDATE courses SET name='$name', code='$code'  WHERE code='$code'";
+
+            if ($connection->query($sql) === TRUE) {
+                return var_dump(http_response_code(200));
+            } else {
+                return var_dump(http_response_code(409));
+            }
+            break;
         }
     }
 ?>
