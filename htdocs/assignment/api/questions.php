093afcbbd424647d74927cb1fcdf9a62a578bd24
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-04-17 15:02:39 +0700

    Create question API

---
+++ htdocs/assignment/api/questions.php
@@ -0,0 +1,53 @@
+﻿<?php
+    require_once('./checkAuth.php');
+?>
+<?php
+    if(isset($_SESSION['token'])){
+        $token = $_SESSION['token'];
+        if(!isLoggedIn($token)){
+            http_response_code(401);
+            return var_dump(http_response_code());
+        }
+    }
+    else {
+        http_response_code(401);
+        return var_dump(http_response_code());
+    }
+    $method = $_SERVER['REQUEST_METHOD'];
+    $data = array();
+    $connection = mysqli_connect("localhost", "root", "", "assignment");
+
+    function isValidCourseCode($courseCode){
+        if (preg_match('/^[A-Z]{2}[0-9]{4}$/', $courseCode))
+            return true;
+        return false;
+    }
+
+    switch($method){
+        case 'POST': {
+            if(!isUserLoggedIn($token)){
+                http_response_code(403);
+                return var_dump(http_response_code());
+            }
+            $code = trim($_POST["code"], " \t\n\r\0\x0B");
+            if(!isValidCourseCode($code)){
+                return var_dump(http_response_code(400));
+            }
+            $question = $_POST["question"];
+            $option1 = $_POST["option1"];
+            $option2 = $_POST["option2"];
+            $option3 = $_POST["option3"];
+            $option4 = $_POST["option4"];
+            $answer = $_POST["answer"];
+            $difficult = $_POST["difficult"];
+            $sql = "INSERT INTO `questions` (code, question, option1, option2, option3, option4, answer, difficult) VALUES ('$code','$question','$option1','$option2','$option3','$option4','$answer','$difficult')";
+           
+            if ($connection->query($sql) === TRUE) {
+                return var_dump(http_response_code(200));
+            } else {
+                return var_dump(http_response_code(409));
+            }
+            break;
+        }
+    }
+?>
--- htdocs/assignment/views/questions/create.php
+++ htdocs/assignment/views/questions/create.php
@@ -2,98 +2,101 @@
 	<?php include_once "../share/head.php" ?>
 	<title>Create Question</title>
 </head>
-<style >
+<style>
 	#create-form {
-		width:50%;
+		width: 50%;
 		margin: 0 auto;
 	}
-	.collapse {
-		margin-right:20%;
-		margin-left: 20%;
-		margin-top:10px;
-	}
-	.form-control{
 
+	.collapse {
+		margin-right: 20%;
+		margin-left: 20%;
+		margin-top: 10px;
 	}
+
+	.form-control {}
 </style>
+
 <body>
-	<div class ="container">
+	<div class="container">
 		<header>
 			<?php include_once "../share/header.php" ?>
 		</header>
-		<div class ="button col-sm-10">
-			<button type="button" class = "btn btn-primary btm-md" data-toggle="collapse" data-target="#demo">Create Question </button>
-			<button type="button" class = "btn btn-primary btm-md" data-toggle="collapse" data-target= "#demo1">View </button>
+		<div class="button col-sm-10">
+			<button type="button" class="btn btn-primary btm-md" data-toggle="collapse" data-target="#demo">Create Question </button>
+			<button type="button" class="btn btn-primary btm-md" data-toggle="collapse" data-target="#demo1">View </button>
 		</div>
-			<div id="demo1" class="collapse">
-				<br>
-				<br>
-				<div class="alert alert-info">
-    				<strong>Please fill the code course and select the difficulty.</strong> 
-  				</div>
-				<label for="codename"> Code Course: </label>
-					<input type="text" class ="form-control" placeholder="Question" name="codename">
-					<br>
-				<label for="difficulty">Difficulty </label>
-      				<select class="form-group" id="difficulty">
-        				<option value="0">0</option>
-        				<option value="1">1</option>
-        				<option value="2">2</option>
-        				<option>All</option>
-     				</select>
-     			<div>
-     				<a href ="view">
-     				<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-     				</a>
-     			</div>
+		<div id="demo1" class="collapse">
+			<br>
+			<br>
+			<div class="alert alert-info">
+				<strong>Please fill the code course and select the difficulty.</strong>
 			</div>
-			<div id="demo" class="collapse">
-				<p style ="font-size: 80px">Create Question</p>
-				 <form class ="form-horizontal" method ="POST" action="/question.php">
+			<label for="codename"> Code Course: </label>
+			<input type="text" class="form-control" placeholder="Question" name="codename">
+			<br>
+			<label for="difficulty">Difficulty </label>
+			<select class="form-group" id="difficulty">
+				<option value="0">0</option>
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option>All</option>
+			</select>
+			<div>
+				<a href="view">
+					<button class="btn btn-default" type="submit">
+						<i class="glyphicon glyphicon-search"></i>
+					</button>
+				</a>
+			</div>
+		</div>
+		<div id="demo" class="collapse">
+			<p style="font-size: 80px">Create Question</p>
+			<form class="form-horizontal" method="POST" action="/api/">
 				<div class="form-group">
 					<label for="question"> Question: </label>
-					<input type="text" class ="form-control" placeholder="Question" name="question">
+					<input type="text" class="form-control" placeholder="Question" name="question" required>
 				</div>
-				<div class ="form-group">
-					<label for="options"> Options: </label>	
-				</div> 
-				<div class ="form-group">
-					<input for="option1" type="text" class="form-control" placeholder="Option 1" name="option1">
-				</div> 
-				<div class ="form-group">
-					<input for="option2" type="text" class="form-control" placeholder="Option 2" name="option2">
+				<div class="form-group">
+					<label for="options"> Options: </label>
 				</div>
-				<div class ="form-group">
-					<input for="option3" type="text" class="form-control" placeholder="Option 3" name="option3">
+				<div class="form-group">
+					<input for="option1" type="text" class="form-control" placeholder="Option 1" name="option1" required>
 				</div>
-				<div class ="form-group">
-					<input for="option4" type="text" class="form-control" placeholder="Option 4" name="option4">
-				</div> 
+				<div class="form-group">
+					<input for="option2" type="text" class="form-control" placeholder="Option 2" name="option2" required>
+				</div>
+				<div class="form-group">
+					<input for="option3" type="text" class="form-control" placeholder="Option 3" name="option3" required>
+				</div>
+				<div class="form-group">
+					<input for="option4" type="text" class="form-control" placeholder="Option 4" name="option4" required>
+				</div>
 				<div class="form-group">
 					<label for="answer"> Answer: </label>
-					<input type="text" class ="form-control" placeholder="Answer" name="answer">
-				</div> 
+					<input type="text" class="form-control" placeholder="Answer" name="answer" required>
+				</div>
 				<div class="form-group">
 					<label for="difficult"> Difficulty: </label>
 					<br>
 					<label class="radio-inline">
-     				 	<input type="radio" value="0" name="optradio">Easy (0)
-    				</label>
-    				<label class="radio-inline">
-      					<input type="radio" value="1" name="optradio">Normal (1)
-    				</label>
-    				<label class="radio-inline">
-      					<input type="radio" value="2" name="optradio">Hard (2)
-      				</label>
+						<input type="radio" value="0" name="difficult" required>Easy (0)
+					</label>
+					<label class="radio-inline">
+						<input type="radio" value="1" name="difficult" required>Normal (1)
+					</label>
+					<label class="radio-inline">
+						<input type="radio" value="2" name="difficult" required>Hard (2)
+					</label>
 				</div>
 				<div class="upload">
 					<button type="submit" class="btn btn-success">Upload</button>
 				</div>
-				</form> 
-			</div>
+			</form>
 		</div>
-		<footer>
-			<?php include_once "../share/footer.php" ?>
-		</footer>
+	</div>
+	<footer>
+		<?php include_once "../share/footer.php" ?>
+	</footer>
 	</div>
 </body>
