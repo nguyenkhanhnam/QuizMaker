910b3682d205e40549b39af122355f8c9e75b1a6
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-05-01 13:09:32 +0700

    Add .htaccess to prevent user to access folder /courses

---
+++ htdocs/assignment/views/error.php
@@ -0,0 +1,36 @@
+﻿<!DOCTYPE html>
+<meta charset="utf-8" />
+<html lang="en">
+
+<head>
+  <?php include_once "share/head.php" ?>
+  <title>Quiz Maker</title>
+</head>
+
+<script src="js/auth/login.js"></script>
+
+<style>
+  #form-center {
+    width: 50%;
+    margin: 0 auto;
+  }
+
+  #forgot {
+    text-decoration: none;
+    display: inline;
+  }
+</style>
+
+<body>
+  <div class="container">
+    <header>
+      <?php include_once "share/header.php" ?>
+    </header>
+    
+    This site is under constructed
+
+    <footer>
+      <?php include_once "share/footer.php" ?>
+    </footer>
+  </div>
+</body>
