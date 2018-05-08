848c25de25177b99e9e3b72154a8fd514596468b
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-05-08 12:59:29 +0700

    Fix Edit Course not working

--- htdocs/assignment/js/courses/detail.js
+++ htdocs/assignment/js/courses/detail.js
@@ -3,7 +3,7 @@
   $('#btn-delete-course').on('click', removeCourse)
 })
 
-function getCourse(courseCode){
+function getCourse(courseCode) {
   $.ajax({
     type: 'GET',
     url: '/api/courses/' + courseCode,
@@ -26,67 +26,63 @@ function getCourse(courseCode){
 function removeCourse() {
   console.log($('#code').val().trim())
   $.ajax({
-      url: '/api/courses/',
-      type: 'DELETE',
-      contentType: 'application/json',
-      data: {
-          code: $('#code-detail').val().trim()
-      },
-      complete: function (res) {
-          console.log(res)
-          if (res.status !== 200) {
-              if (res.status === 500) {
-                  var str = res.responseText.trim()
-                  var data = JSON.parse(str)
-                  displayToast('error', data.message)
-              }
-          } else {
-              var str = res.responseText.trim()
-              var data = JSON.parse(str)
-              displayToast('success', data.message)
-              getCourses()
-          }
-      }
-  })
-}
-
-function editCourse(){
-  var $form = $('edit-form-course')
-  var data = {
-    code: $('#code-detail').val(),
-    name: $('#name-detail').val()
-  }
-  $form.submit(function () {
-    $.ajax({
-      url: '/api/courses/',
-      type: 'PUT',
-      contentType: 'application/json',
-      data: data,
-      complete: function (res) {
-        if (res.status !== 200) {
-          if(res.status === 400){
-            displayToast('error', 'Invalid data')
-          }
-          else if (res.status === 404) {
-            displayToast('error', 'Course not found')
-          } 
-          else if (res.status === 409) {
-            displayToast('error', 'Course code existed')
-          }
-		  else{
-			displayToast('error', 'khong biet cai gi dang xay ra')
-		  }
-        } else {
-          displayToast('success', 'Course edited successfully')
-          getCourses()
+    url: '/api/courses/',
+    type: 'DELETE',
+    contentType: 'application/json',
+    data: {
+      code: $('#code-detail').val().trim()
+    },
+    complete: function (res) {
+      console.log(res)
+      if (res.status !== 200) {
+        if (res.status === 500) {
+          var str = res.responseText.trim()
+          var data = JSON.parse(str)
+          displayToast('error', data.message)
         }
+      } else {
+        var str = res.responseText.trim()
+        var data = JSON.parse(str)
+        displayToast('success', data.message)
+        getCourses()
       }
-    })
-    return false
+    }
   })
 }
 
-function showButtonCourse(){
+function editCourse() { // TODO: Khong lay course code tu input nua
+  var $form = $('#edit-form-course')
+  var data = getFormData($form)
+  data.code = $('#code').val().trim()
+  $.ajax({
+    url: '/api/courses/',
+    type: 'PUT',
+    contentType: 'application/json',
+    data: data,
+    complete: function (res) {
+      console.log(res)
+      if (res.status !== 200) {
+        if (res.status === 400) {
+          displayToast('error', 'Invalid data')
+        }
+        else if (res.status === 404) {
+          displayToast('error', 'Course not found')
+        }
+        else if (res.status === 409) {
+          displayToast('error', 'Course code existed')
+        }
+        else {
+          displayToast('error', 'khong biet cai gi dang xay ra')
+        }
+      } else {
+        displayToast('success', 'Course edited successfully')
+        getCourses()
+      }
+    }
+  })
+}
+
+function showButtonCourse() {
   $('#btn-save-course').show()
   $('#btn-delete-course').show()
 }
