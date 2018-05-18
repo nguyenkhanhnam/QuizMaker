1b11bfc0b3577a951fa731757e81d285af2ae631
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-05-18 22:38:40 +0700

    Fix Login (2)

--- htdocs/assignment/js/auth/login.js
+++ htdocs/assignment/js/auth/login.js
@@ -1,36 +1,28 @@
 ﻿$(document).ready(function () {
-    $('#btn-login').on('click', function () {
-        var username = $("#username").val()
-        var password = $("#password").val()
-        var loginData = {
-            username: username,
-            password: password
-        }
-
-        if (!username) {
+    $('form').on('submit', function (e) {
+        e.preventDefault()
+        const loginData = getFormData($('form'))
+        if (!loginData.username) {
             return displayToast('error', 'Username is required')
         }
 
-        if (!password) {
+        if (!loginData.password) {
             return displayToast('error', 'Password is required')
         }
-
-        $('#login-form').submit(function (e) {
-            e.preventDefault()
-            $.ajax({
-                type: 'POST',
-                url: '/api/auth',
-                data: loginData,
-                complete: function (res) {
-                    const message = JSON.parse(res.responseText.trim()).message
-                    if (res.status === 200){
-                        return displayToastWithRedirect('success', message, '/')
-                    }
-                    else {
-                        return displayToast('error', message)
-                    }
+        
+        $.ajax({
+            type: 'POST',
+            url: '/api/auth',
+            data: loginData,
+            complete: function (res) {
+                const message = JSON.parse(res.responseText.trim()).message
+                if (res.status === 200){
+                    return displayToastWithRedirect('success', message, '/')
                 }
-            })
+                else {
+                    return displayToast('error', message)
+                }
+            }
         })
     })
 }
--- htdocs/assignment/views/login.php
+++ htdocs/assignment/views/login.php
@@ -1,19 +1,9 @@
-﻿<!DOCTYPE html>
-<meta charset="utf-8" />
-<html lang="en">
-
-<head>
+﻿<head>
   <?php include_once "share/head.php" ?>
   <title>Quiz Maker</title>
 </head>
 
-<script src="js/auth/login.js"></script>
-
 <style>
-  #form-center {
-    margin: 0 auto;
-  }
-
   #forgot {
     text-decoration: none;
     display: inline;
@@ -27,15 +17,15 @@
     </header>
 
     <div class="row">
-      <div class="col-xs-12" id="form-center">
-        <form id="login-form">
+      <div class="col-xs-12">
+        <form>
           <div class="form-group">
             <label for="username">Username</label>
             <div class="input-group">
               <span class="input-group-addon">
                 <i class="fa fa-user-circle fa-lg" aria-hidden="true"></i>
               </span>
-              <input type="text" class="form-control" id="username" placeholder="Username" name="username" required>
+              <input type="text" class="form-control" placeholder="Username" name="username">
             </div>
           </div>
           <div class="form-group">
@@ -44,14 +34,14 @@
               <span class="input-group-addon">
                 <i class="fa fa-key fa-lg" aria-hidden="true"></i>
               </span>
-              <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
+              <input type="password" class="form-control" placeholder="Password" name="password" required>
             </div>
           </div>
           <div class="pull-left">
             <a id="forgot" href="#">Forgot password?</a>
           </div>
           <div class="text-right">
-            <button type="submit" id="btn-login" class="btn btn-primary">
+            <button type="submit" class="btn btn-primary">
               <i class="fa fa-sign-in" aria-hidden="true"></i> Log In</button>
           </div>
         </form>
@@ -63,3 +53,5 @@
     </footer>
   </div>
 </body>
+
+<script src="js/auth/login.js"></script>
