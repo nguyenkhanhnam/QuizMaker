a6ee61eda92507e582102f776b17849d65a528c9
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-05-19 08:20:55 +0700

    Add About page and QuizMakerGuide

---
+++ htdocs/assignment/about.php
@@ -0,0 +1,15 @@
+﻿<?php
+    include_once "./views/share/head.php";
+?>
+
+<title>About</title>
+
+<header>
+    <?php include_once "./views/share/header.php" ?>
+</header>
+
+<?php include_once "./views/about.html" ?>
+
+<footer>
+    <?php include_once "./views/share/footer.php" ?>
+</footer>
