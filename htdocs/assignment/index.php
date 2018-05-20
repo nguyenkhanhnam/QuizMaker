37b0be1e6ef38a0f787c5a7c867b61a19b15986c
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-05-20 11:42:41 +0700

    Fix unexpected redirect?

--- htdocs/assignment/index.php
+++ htdocs/assignment/index.php
@@ -2,6 +2,32 @@
     include_once "./views/share/head.php";
 ?>
 
+<?php
+    if(isset($_SESSION['token']) && $_SESSION['token']!=''){
+      $token = $_SESSION['token'];
+      if(isLoggedIn($token)){
+        $role = $_SESSION['role'];
+        switch($role){
+            case 0:
+                header('location:/dashboard/admin');
+                break;
+            case 1:
+                header('location:/dashboard/user');
+                break;
+            case 2:
+                header('location:/dashboard/staff');
+                break;
+            }
+      }
+      else {
+        echo "<script>$('#login-modal').modal('show')</script>";
+      }
+    }
+    else {
+        echo "<script>$('#login-modal').modal('show')</script>";
+    }
+?>
+
 <title>QuizMaker</title>
 
 <header>
