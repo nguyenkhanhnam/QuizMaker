bcbdda530a72736da9cc0da48b1274f86296c851
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-05-20 10:17:29 +0700

    Add profile.php

---
+++ htdocs/assignment/profile.php
@@ -0,0 +1,30 @@
+﻿<?php
+    include_once "./views/share/head.php";
+?>
+
+<?php
+    if(isset($_SESSION['token']) && $_SESSION['token']!=''){
+      $token = $_SESSION['token'];
+      if(isLoggedIn($token)){
+        
+      }
+      else {
+        return header('location:/');
+      }
+    }
+    else {
+        return header('location:/');
+    }
+?>
+
+<title>QuizMaker</title>
+
+<header>
+    <?php include_once "./views/share/header.php" ?>
+</header>
+
+<?php include_once "./views/accounts/detail.html" ?>
+
+<footer>
+    <?php include_once "./views/share/footer.php" ?>
+</footer>
