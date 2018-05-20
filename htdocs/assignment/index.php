32e09399d942a4e4a3f2dfab5569356730c430ed
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-05-20 11:43:29 +0700

    Delete unused code

--- htdocs/assignment/index.php
+++ htdocs/assignment/index.php
@@ -43,20 +43,7 @@
 <?php
     if(isset($_SESSION['token']) && $_SESSION['token']!=''){
       $token = $_SESSION['token'];
-      if(isLoggedIn($token)){
-        $role = $_SESSION['role'];
-        switch($role){
-            case 0:
-                header('location:/dashboard/admin');
-                break;
-            case 1:
-                header('location:/dashboard/user');
-                break;
-            case 2:
-                header('location:/dashboard/staff');
-                break;
-            }
-      }
+      if(isLoggedIn($token)){}
       else {
         echo "<script>$('#login-modal').modal('show')</script>";
       }
