befbe058011851c8dcaba22e8b8482ea5a550872
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-05-24 22:01:12 +0700

    Fix UI Profile page

--- htdocs/assignment/profile.php
+++ htdocs/assignment/profile.php
@@ -17,14 +17,18 @@
     }
 ?>
 
-<title>QuizMaker</title>
+<title>Profile</title>
 
+<body class="container">
 <header>
     <?php include_once "./views/share/header.php" ?>
 </header>
+
+<h1 style="font-family: Montserrat, sans-serif" class="text-center">Profile</h1>
 
 <?php include_once "./views/profile.html" ?>
 
 <footer>
     <?php include_once "./views/share/footer.php" ?>
 </footer>
+</body>
